56b28f3fa94680dee702784b056692888bd09ab6
sebrahem605-lang <sebrahem605@gmail.com>
2026-06-13 01:00:03 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+class NotesApp{
+    constructor(){
+        this.textareaNote = document.getElementById('noteText');
+        this.addBtnNote = document.getElementById('addBtn');
+        this.textColorNote = document.getElementById('text-color');
+        this.bgColorNote = document.getElementById('bg-color');
+        this.notesContainer = document.getElementById('notes-container');
+        this.event();
+    }
+
+    event(){
+     this.addBtnNote.addEventListener('click',(e)=>{
+        this.addNotes();
+     });
+    }
+
+    addNotes(){
+    const textValue = this.textareaNote.value.trim();
+    if(textValue === ''){
+      alert("Please write a note first");
+      return;
+    }
+
+    const textColorValue =this.textColorNote.value;
+    const bgColorValue =this.bgColorNote.value;
+
+    this.renderNotes(textValue, textColorValue, bgColorValue);
+    this.textareaNote.value = "";
+    }
+
+    renderNotes(textValue, textColorValue, bgColorValue){
+    //  creat a div
+    const noteCard = document.createElement('div');
+    noteCard.classList.add('note-card');
+    noteCard.style.backgroundColor=bgColorValue;
+    noteCard.style.color=textColorValue;
+    noteCard.textContent=textValue;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+    deleteBtn.style.cursor = 'pointer'
+
+    deleteBtn.addEventListener('click',()=>{
+        noteCard.remove();
+    });
+    noteCard.appendChild(deleteBtn);
+    this.notesContainer.appendChild(noteCard);
+    }
+}
+
+const app = new NotesApp();
